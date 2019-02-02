cfa5abae84f64e86779f09121d17c1f562d870d4
Andrew <andrewmoreno1431@gmail.com>
2019-02-01 17:15:03 -0800

    Added comments to OrderStatus

--- Order_Info/OrderStatus.py
+++ Order_Info/OrderStatus.py
@@ -1,5 +1,6 @@
 import openpyxl
 
+#Function to change status of order.
 def status_change():
     complete=input("Is the transaction complete? (y or n) ")
     returned=input("What is the status of the return? (p or s or f)")
@@ -15,10 +16,12 @@ def status_change():
     else:
         return "stop being a dumbass"
 
-
+#Opens SD_Order_info.xlsx in sheet 1
 xfile = openpyxl.load_workbook('SD_Order_info.xlsx')
 sheet = xfile["Sheet1"]
 
+#Changes single cell to string given by Function
+#Column value stays fixed, row value needs to be changed to update as new entries added 
 sheet.cell(row=6,column=10).value = status_change()
 
-xfile.save('test_SD_Order_info.xlsx')
+xfile.save('SD_Order_info.xlsx')
