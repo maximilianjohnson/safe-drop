a2fef2c3c873b77f033fe2f3236888f491de09de
Andrew <andrewmoreno1431@gmail.com>
2019-02-09 11:20:06 -0800

    Created user profiles GUI

--- User_Profiles/User_Profiles_DB.py
+++ User_Profiles/User_Profiles_DB.py
@@ -1,8 +1,8 @@
 """
 A program that creates an SQL database for new user profiles
 and adds new entries with this info:
-first name, last name, username, email, age, postal code,
-address, city, province, country
+first name, last name, username, email, age, date joined, address, city,
+province, country, postal code, distance, txa, user_rating, status
 
 """
 #FOR PROTOTYPING ONLY, TO BE EXPANDED UPON FOR WEB INTEGRATION/POSTGRESQL
@@ -19,10 +19,11 @@ import user_distance
 def create_up_db():
     conn=sqlite3.connect("User_ProfileDataBase.db")
     cur=conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS users (first_name TEXT,\
-    last_name TEXT, user_name TEXT, email TEXT, age INTEGER, date_joined DATE,\
-    address TEXT, city TEXT, province TEXT, country TEXT, postal_code TEXT, \
-    distance  FLOAT, txA INTEGER, user_rating INTEGER, status TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, \
+                first_name TEXT, last_name TEXT, user_name TEXT, email TEXT, \
+                age INTEGER, date_joined DATE, address TEXT, city TEXT, \
+                province TEXT, country TEXT, postal_code TEXT, distance FLOAT, \
+                txA INTEGER, user_rating INTEGER, status TEXT)")
     conn.commit()
     conn.close()
 
@@ -33,7 +34,7 @@ def insert_newprofile_up_db(fn, ln, un, em, age, ad, city, prov, ctry, pc):
     date = datetime.now()
     conn=sqlite3.connect("User_ProfileDataBase.db")
     cur=conn.cursor()
-    cur.execute("INSERT INTO users VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, \
+    cur.execute("INSERT INTO users VALUES(NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, \
     ?, ?, ?, ?)", (fn, ln, un, em, age, date, ad, city, prov, ctry, pc, \
     user_distance.get_distance(ad,city), 0, 5, "Good Standing"))
     conn.commit()
@@ -48,6 +49,44 @@ def view():
     conn.close()
     return rows
 
+def search(fn="", ln="", un="", em="", age="", dj="", ad="", city="", prov="", \
+           ctry="", pc="", dist="", txa="", ur="", status=""):
+    conn=sqlite3.connect("User_ProfileDataBase.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM users WHERE first_name=? OR last_name=? OR \
+                user_name=? OR email=? OR age=? OR date_joined=? OR address=? \
+                OR city=? OR province=? OR country=? OR postal_code=? OR \
+                distance=? OR txA=? OR user_rating=? OR status=?", \
+                (fn, ln, un, em, age, dj, ad, city, prov, ctry, pc, dist, \
+                txa, ur, status))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn=sqlite3.connect("User_ProfileDataBase.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM users WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+def update(fn, ln, un, em, age, dj, ad, city, prov, ctry, pc, txa, ur, \
+            status):
+    conn=sqlite3.connect("User_ProfileDataBase.db")
+    cur_conn.cursor()
+    cur.execute("UPDATE users SET first_name=? OR last_name=? OR \
+                user_name=? OR email=? OR age=? OR date_joined=? OR address=? \
+                OR city=? OR province=? OR country=? OR postal_code=? OR \
+                distance=? OR txA=? OR user_rating=? OR status=?", \
+                (fn, ln, un, em, age, dj, ad, city, prov, ctry, pc, \
+                txa, ur, status))
+    conn.close()
+
+
+
+
+
+'''
 #prototype using cmd line inputs
 fn = input("Enter your first name: ")
 ln = input("Enter your last name: ")
@@ -63,3 +102,5 @@ pc = input("Enter your postal code: ")
 create_up_db()
 insert_newprofile_up_db(fn, ln, un, em, age, ad, city, prov, ctry, pc)
 print(view())
+'''
+create_up_db()
---
+++ User_Profiles/User_Profiles_Frontend.py
@@ -0,0 +1,228 @@
+
+#FOR PROTOTYPING ONLY, TO BE EXPANDED UPON FOR WEB INTEGRATION/POSTGRESQL
+#Author: Andrew Moreno
+#Date: Feb 8th
+
+from tkinter import *
+import User_Profiles_DB
+
+
+def get_selected_row(event):
+    try:
+        global selected_tuple
+        index=list1.curselection()[0]
+        selected_tuple=list1.get(index)
+        e1.delete(0, END)
+        e1.insert(END, selected_tuple[1])
+        e2.delete(0, END)
+        e2.insert(END, selected_tuple[2])
+        e3.delete(0, END)
+        e3.insert(END, selected_tuple[3])
+        e4.delete(0, END)
+        e4.insert(END, selected_tuple[4])
+        e5.delete(0, END)
+        e5.insert(END, selected_tuple[5])
+        e6.delete(0, END)
+        e6.insert(END, selected_tuple[6])
+        e7.delete(0, END)
+        e7.insert(END, selected_tuple[7])
+        e8.delete(0, END)
+        e8.insert(END, selected_tuple[8])
+        e9.delete(0, END)
+        e9.insert(END, selected_tuple[9])
+        e10.delete(0, END)
+        e10.insert(END, selected_tuple[10])
+        e11.delete(0, END)
+        e11.insert(END, selected_tuple[11])
+        e12.delete(0, END)
+        e12.insert(END, selected_tuple[12])
+        e13.delete(0, END)
+        e13.insert(END, selected_tuple[13])
+        e14.delete(0, END)
+        e14.insert(END, selected_tuple[14])
+        e15.delete(0, END)
+        e15.insert(END, selected_tuple[15])
+    except IndexError:
+        pass
+
+
+def view_command():
+    list1.delete(0, END)
+    for row in User_Profiles_DB.view():
+        list1.insert(END,row)
+
+def search_command():
+    list1.delete(0, END)
+    for row in User_Profiles_DB.search(firstname_text.get(), lastname_text.get(),\
+        username_text.get(), email_text.get(), age_text.get(), \
+        datejoined_text.get(), address_text.get(), city_text.get(), province_text.get(), \
+        country_text.get(), postalcode_text.get(), distance_value.get(), txa_value.get(), \
+        userrating_value.get(), status_text.get()):
+        list1.insert(END,row)
+
+def newuser_command():
+    User_Profiles_DB.insert_newprofile_up_db(firstname_text.get(), lastname_text.get(),\
+        username_text.get(), email_text.get(), age_text.get(), \
+        address_text.get(), city_text.get(), province_text.get(), \
+        country_text.get(), postalcode_text.get())
+    list1.delete(0, END)
+    view_command()
+
+def delete_command():
+    User_Profiles_DB.delete(selected_tuple[0])
+    view_command()
+
+def update_command():
+    User_Profiles_DB.update(firstname_text.get(), lastname_text.get(),\
+        username_text.get(), email_text.get(), age_text.get(), \
+        datejoined_text.get(), address_text.get(), city_text.get(), province_text.get(), \
+        country_text.get(), postalcode_text.get(), txa_value.get(), \
+        userrating_value.get(), status_text.get(), selected_tuple[0])
+
+
+window = Tk()
+
+l1 = Label(window, text="First Name")
+l1.grid(row=0, column=0)
+
+l2 = Label(window, text="Last Name")
+l2.grid(row=0, column=2)
+
+l3 = Label(window, text="Username")
+l3.grid(row=0, column=4)
+
+l4 = Label(window, text="Email")
+l4.grid(row=0, column=6)
+
+l5 = Label(window, text="Age")
+l5.grid(row=0, column=8)
+
+l6 = Label(window, text="Date Joined")
+l6.grid(row=0, column=10)
+
+l7 = Label(window, text="Address")
+l7.grid(row=0, column=12)
+
+l8 = Label(window, text="City")
+l8.grid(row=0, column=14)
+
+l9 = Label(window, text="Province")
+l9.grid(row=1, column=0)
+
+l10 = Label(window, text="Country")
+l10.grid(row=1, column=2)
+
+l11 = Label(window, text="Postal Code")
+l11.grid(row=1, column=4)
+
+l12 = Label(window, text="Distance")
+l12.grid(row=1, column=6)
+
+l13 = Label(window, text="TXA")
+l13.grid(row=1, column=8)
+
+l14 = Label(window, text="User Rating")
+l14.grid(row=1, column=10)
+
+l15 = Label(window, text="Status")
+l15.grid(row=1, column=12)
+
+
+firstname_text = StringVar()
+e1 = Entry(window, textvariable=firstname_text)
+e1.grid(row=0, column=1)
+
+lastname_text = StringVar()
+e2 = Entry(window, textvariable=lastname_text)
+e2.grid(row=0, column=3)
+
+username_text = StringVar()
+e3 = Entry(window, textvariable=username_text)
+e3.grid(row=0, column=5)
+
+email_text = StringVar()
+e4 = Entry(window, textvariable=email_text)
+e4.grid(row=0, column=7)
+
+age_text = StringVar()
+e5 = Entry(window, textvariable=age_text)
+e5.grid(row=0, column=9)
+
+datejoined_text = StringVar()
+e6 = Entry(window, textvariable=datejoined_text)
+e6.grid(row=0, column=11)
+
+address_text = StringVar()
+e7 = Entry(window, textvariable=address_text)
+e7.grid(row=0, column=13)
+
+city_text = StringVar()
+e8 = Entry(window, textvariable=city_text)
+e8.grid(row=0, column=15)
+
+province_text = StringVar()
+e9 = Entry(window, textvariable=province_text)
+e9.grid(row=1, column=1)
+
+country_text = StringVar()
+e10 = Entry(window, textvariable=country_text)
+e10.grid(row=1, column=3)
+
+postalcode_text = StringVar()
+e11 = Entry(window, textvariable=postalcode_text)
+e11.grid(row=1, column=5)
+
+distance_value = StringVar()
+e12 = Entry(window, textvariable=distance_value)
+e12.grid(row=1, column=7)
+
+txa_value = StringVar()
+e13 = Entry(window, textvariable=txa_value)
+e13.grid(row=1, column=9)
+
+userrating_value = StringVar()
+e14 = Entry(window, textvariable=userrating_value)
+e14.grid(row=1, column=11)
+
+status_text = StringVar()
+e15 = Entry(window, textvariable=status_text)
+e15.grid(row=1, column=13)
+
+
+
+list1=Listbox(window, height=6, width=230)
+list1.grid(row=2, column=0, rowspan=5, columnspan=14)
+
+sb1=Scrollbar(window, orient = VERTICAL)
+sb1.grid(row=2, column=14, rowspan=5, sticky=N+S+W)
+
+sb2=Scrollbar(window, orient = HORIZONTAL)
+sb2.grid(row=7, column=0, columnspan=14, sticky=W+E+N)
+
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
+list1.configure(xscrollcommand=sb2.set)
+sb2.configure(command=list1.xview)
+
+list1.bind('<<ListboxSelect>>', get_selected_row)
+
+b1=Button(window, text="View All", width=12, command=view_command)
+b1.grid(row=2, column=15)
+
+b2=Button(window, text="Search Entry", width=12, command=search_command)
+b2.grid(row=3, column=15)
+
+b3=Button(window, text="Add New User", width=12, command=newuser_command)
+b3.grid(row=4, column=15)
+
+b4=Button(window, text="Update Selected", width=12, command=update_command)
+b4.grid(row=5, column=15)
+
+b5=Button(window, text="Delete Selected", width=12, command=delete_command)
+b5.grid(row=6, column=15)
+
+b6=Button(window, text="Close", width=12, command=window.destroy)
+b6.grid(row=7, column=15)
+
+window.mainloop()
