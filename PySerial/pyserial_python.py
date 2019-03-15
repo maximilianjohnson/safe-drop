04f87b54c38ec99935668ca5633ca315d33dd525
Haske <sumaila.haske@gmail.com>
2019-03-14 23:19:37 -0700

    Completed code for keypad and code generation.

---
+++ PySerial/pyserial_python.py
@@ -0,0 +1,56 @@
+import serial #Imports PySerial for sending information to and from Arduino.
+import time #Imports time.
+import random # imports function to generate random number.
+import struct
+ser = serial.Serial("COM4",9600) #COM4 is the port for the Arduino. Change to appropriate port on your computer. Second number is baudrate. Match to rate in Arduino.
+
+time.sleep(2) #Adds 5 second delay before Python starts searching for a signal from the Arduino.
+var = ''
+codeStr = ''
+codeLen = 0
+checkVal = 0
+cond = True
+code = []
+codeByte = []
+returned = []
+
+def code_gen(codeByte, codeNum):
+    num = 0
+    i = 0
+    numStr = ''
+    numToBytes = 0
+    while (i < 6):
+        num = random.randint(0,9)
+        numStr = str(num)
+        codeNum.append(numStr)
+        numToBytes = bytes(numStr, encoding='utf-8')
+        codeByte.append(numToBytes)
+        i += 1
+    return
+
+
+while cond == True:
+    ser.flushInput()
+    code_gen(codeByte, code)
+    codeLen = len(code)
+    codeStr = ''.join(code)
+    print('Your code for this instance is: ' + codeStr)
+    print("Sending code to Arduino for verification...")
+    for i in range(codeLen):
+        ser.write(codeByte[i])
+    time.sleep(10)
+    while ser.inWaiting():
+        var = ser.readline()[0:1]
+        var = var.decode('utf-8')
+        returned.append(var)
+    try:
+        checkVal = int(returned[0])
+        print(checkVal)
+    except:
+        print("You did not enter the correct number of values in the keypad.")
+    if checkVal == len(code):
+        print ("This code is correct! The door will now be unlocked.")
+        print ("The scale is now active.")
+    else:
+        print ("This code is not correct.")
+    cond = False
--- PySerial/pyserial_test.py
+++ PySerial/pyserial_test.py
@@ -1,16 +1,17 @@
 import serial #Imports PySerial for sending information to and from Arduino.
 import time #Imports time.
 import random # imports function to generate random number.
+import struct
 ser = serial.Serial("COM4",9600) #COM4 is the port for the Arduino. Change to appropriate port on your computer. Second number is baudrate. Match to rate in Arduino.
-time.sleep(5) #Adds 5 second delay before Python starts searching for a signal from the Arduino.
+time.sleep(2) #Adds 5 second delay before Python starts searching for a signal from the Arduino.
 cond = True
+codeLoop = False
 while cond == True:
-    #code = random(100000,999999) (WIP - Need keypad to test entering the code)
+    #code = random(100000,999999) #(WIP - Need keypad to test entering the code)
     #print(code)
+    tester = 900
+    testerBin = (struct.pack('>H', tester))
     print("Sending code to Arduino for verification...")
-    ser.write(b'8') #Writes the number 8 in binary to the serial port for the Arduino to read.
-    cond = input("Would you like to repeat?") #Conditional to continue the loop.
-    if cond == 'y':
-        cond = True
-    else:
-        cond = False
+    print (testerBin)
+    ser.write(testerBin) #Writes the number 8 in binary to the serial port for the Arduino to read
+    cond = False
